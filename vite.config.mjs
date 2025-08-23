c931c89ac5e5987ff3b38ad1cdf248a87565dfb0
Awanish-26 <awanishyadav996@gmail.com>
2025-08-23 22:59:49 +0530

    fix3

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/folio/',
     plugins: [react(), tailwindcss()],
 });
