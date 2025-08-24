2e7b633a68e05062d672807cea9a7ba62642e831
Awanish-26 <awanishyadav996@gmail.com>
2025-08-24 17:30:54 +0530

    speed insights added

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import Sidebar from './Components/Sidebar';
 import { Analytics } from "@vercel/analytics/react"
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Analytics />
+    <SpeedInsights />
     <Sidebar />
     <App />
   </React.StrictMode>
